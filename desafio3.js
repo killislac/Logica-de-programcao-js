a6aa1f6ffe78bf868941cc72fcbace923fb85241
Kamily Lacerda de Carvalho <123599793+killislac@users.noreply.github.com>
2024-03-19 14:28:34 +0000

    js

--- desafio3.js
+++ desafio3.js
@@ -1,10 +1,38 @@
-const temperatura = window.prompt('Temperartura atual:');
-const escala = window.prompt('Escolha a escala:');
-const conversao = window.prompt ('Escolha a temperatura a ser covertida F, K ou C');
-let calculo;
+let temperatura = parseFloat(window.prompt('Digite a temperatura:'));
+const escala = window.prompt('Indique qual é a escala:').toUpperCase();
+const conversao = window.prompt('Escala a ser convertida:F , K, C').toUpperCase();
+console.log(`O valor não é NaN: ${!isNaN(temperatura)}`);
 
-if (escala === 'C' && conversao ==='F'){
-    calculo = (temperatura * 9/5) = 32
-    window.alert(`Celsius para Fahrenheit: ${calculo} °F`)
+
+if(escala == 'C' && conversao == 'F' ){
+    calculoFahrenhreit = (temperatura * 9/5) + 32;
+    window.alert(`Celsius para Fahrenhreit:${calculoFahrenhreit} °F`);
+
+}else if (escala == 'F'  && conversao == 'C' ){
+    calculoCelsius = (temperatura - 32) * 5/9;
+    window.alert(`Fahrenhereit para Celsius: ${calculoCelsius} °C`);
+
+} else if (escala == 'K'  && conversao == 'C'){
+    calculoCelsius2 = temperatura - 273.15;
+    window.alert(`Kelvin para Celsius: ${calculoCelsius2} °C`);
+
+} else if (escala == 'K'  && conversao == 'F' ){
+    calculofah = temperatura * 9/5 - 459.67;
+    window.alert(`Kelvin para Fahrenhereit: ${calculofah} °F`);
+
+} else if (escala == 'C'  && conversao == 'K'){
+    calculocel = (temperatura * 1) + 273.15;
+    window.alert(`Celsius para Kelvin: ${calculocel} °K`);
+
+} else if (escala == 'F'  && conversao == 'K'){
+    calculokelvin = ((temperatura *1) + 459.67) * 5/9;
+    window.alert(`Fahrenhereit para Kelvin: ${calculokelvin} °K`);
+} else if (escala != 'C' || 'F' ||'K'){
+    window.alert('Escala está incorreta!');
+}else if (conversao != 'C' ||escala !='F' || escala != 'K'){
+    window.alert('Conversao está incorreta!');
+}else if (temperatura && escala && conversao != 'C' || 'F' || 'K'){
+    window.alert('Todas as entradas estão erradas!');
 }
+
 
