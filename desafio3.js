4258a2d4c47ae30135a804d7494b00bc80ad6896
Kamily <kamilylacerda1@gmail.com>
2024-03-28 11:49:16 -0300

    Finalizado

--- desafio3.js
+++ desafio3.js
@@ -1,48 +1,44 @@
-function converteTEM(){
-    let temperatura = parseFloat(window.prompt('Digite a temperatura:'));
-    if(!isNaN(temperatura)){
-        const escala = window.prompt('Indique qual é a escala:').toUpperCase();
-        if (escala === 'C' || escala === 'F' || escala === 'K') {
-            const conversao = window.prompt('Escala a ser convertida:F , K, C').toUpperCase();
-            if (conversao === 'C' || conversao === 'F' || conversao === 'K') {  
+function converteTEM(temperatura,escala,conversao){
+    let temperaturaConvertida;
+    if(!isNaN(temperatura));
+        escala = escala.toUpperCase();
+        conversao = conversao.toUpperCase();
+   
+    if (escala === 'C' || escala === 'F' || escala === 'K') {
+        if (conversao === 'C' || conversao === 'F' || conversao === 'K') {  
+                
                 if(escala != conversao){
                     if(escala == 'C' && conversao == 'F' ){
                         calculoFahrenhreit = (temperatura * 9/5) + 32;
-                        window.alert(`Celsius para Fahrenhreit:${calculoFahrenhreit} °F`);
-    
+                       temperaturaConvertida =`Celsius para Fahrenhreit:${calculoFahrenhreit} °F`;
+                       
                     }else if (escala == 'F'  && conversao == 'C' ){
                         calculoCelsius = (temperatura - 32) * 5/9;
-                        window.alert(`Fahrenhereit para Celsius: ${calculoCelsius} °C`);
+                      temperaturaConvertida =`Fahrenhereit para Celsius: ${calculoCelsius} °C`;
     
                     }else if (escala == 'K'  && conversao == 'C'){
                         calculoCelsius2 = temperatura - 273.15;
-                        window.alert(`Kelvin para Celsius: ${calculoCelsius2} °C`);
+                       temperaturaConvertida = `Kelvin para Celsius: ${calculoCelsius2} °C`;
     
                     }else if (escala == 'K'  && conversao == 'F' ){
                         calculofah = temperatura * 9/5 - 459.67;
-                        window.alert(`Kelvin para Fahrenhereit: ${calculofah} °F`);
+                       temperaturaConvertida = `Kelvin para Fahrenhereit: ${calculofah} °F`;
     
                     }else if (escala == 'C'  && conversao == 'K'){
                         calculocel = (temperatura * 1) + 273.15;
-                        window.alert(`Celsius para Kelvin: ${calculocel} °K`);
+                       temperaturaConvertida = `Celsius para Kelvin: ${calculocel} °K`;
     
                     }else if (escala == 'F'  && conversao == 'K'){
                         calculokelvin = ((temperatura *1) + 459.67) * 5/9;
-                        window.alert(`Fahrenhereit para Kelvin: ${calculokelvin} °K`);
+                       temperaturaConvertida = `Fahrenhereit para Kelvin: ${calculokelvin} °K`;
                     }
                 }else{
-                    window.alert(`Escala selecionada: ${escala}. Selecione uma escala diferente.`)
+                   temperaturaConvertida = `Escala selecionada: ${escala}. Selecione uma escala diferente.`
                 }
-            }else {
-                window.alert('Você digitou uma conversão inválida. Recarregue a página e tente novamente');
             }
     
-        }else {
-        window.alert('Você digitou uma escala inválida. Recarregue a página e tente novamente');
         }
-    
-    }else {
-    window.alert('Você digitou uma temperatura inválida. Recarregue a página e tente novamente');
+        
+        return temperaturaConvertida;
     }
 
-}
