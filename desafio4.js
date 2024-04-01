0ea291c3189af3ebc12d7b1d11967dfb58877dd8
Kamily <kamilylacerda1@gmail.com>
2024-04-01 09:22:18 -0300

    Atualizacao

---
+++ desafio4.js
@@ -0,0 +1,9 @@
+function somaTriangulo(a, b ,c){
+    const ladoA = a, ladoB = b, ladoC = c
+    let mensagem;
+    if ((ladoA === ladoB) && ladoC){
+        mensagem = 'triângulo é equilátero';
+       
+       }
+       return mensagem;
+}
